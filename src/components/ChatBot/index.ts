5fad9adbd2fd4e212f29a2bd0da948dde84304f3
dani <daniyal12335@hotmail.com>
2025-12-21 10:04:06 +0500

    chore: backend-only branch - code only

--- src/components/ChatBot/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * ChatBot Components Index
- * 
- * Re-exports all chatbot-related components.
- */
-
-export { ChatBot } from './ChatBot';
-export { RateLimitBanner } from './RateLimitBanner';
